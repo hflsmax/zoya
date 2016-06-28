ae716b6740e5c96c6defd592bee915858f61a9b9
Max Ma <hflsmax@gmail.com>
2016-06-27 22:59:36 -0400

    Integrated pages

--- saving_app/urls.py
+++ saving_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 # Create your views here.
 urlpatterns = [
     # url(r'^(?P<user_id>[0-9]+)/$', views.first_page, name='index'),
-    url(r'^$', views.first_page, name='index'),
+    url(r'^$', views.choice_without_id, name = "choice_without_id"),
+    url(r'^([0-9]*)$', views.choice, name = "choice"),
+    url(r'^set/([0-9]*)$', views.set, name = "set"),
 ]
--- saving_app/views.py
+++ saving_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
-def first_page(request):
+def choice_without_id(request):
     # return render(request, 'saving_app/voya-choices.html', {'user_id': user_id})
-    return render(request, 'saving_app/voya-choices.html')
+    print "no id"
+    return render(request, 'saving_app/voya-choices.html', {"user_id": "no id"})
+
+def choice(request, user_id):
+    # return render(request, 'saving_app/voya-choices.html', {'user_id': user_id})
+    print "have id"
+    return render(request, 'saving_app/voya-choices.html', {"user_id": user_id})
+
+def set(request, user_id):
+    # return render(request, 'saving_app/voya-choices.html', {'user_id': user_id})
+    return render(request, 'saving_app/voya-set.html', {"user_id": user_id})
