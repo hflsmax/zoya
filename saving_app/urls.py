cbe74f177c149af4144426a6ad8ead76a32441ee
Max Ma <hflsmax@gmail.com>
2016-07-09 15:40:52 -0400

    Page warm closure

--- saving_app/urls.py
+++ saving_app/urls.py
@@ -4,8 +4,6 @@ from . import views
 # Create your views here.
 urlpatterns = [
     url(r'^$', views.choice_without_id, name = "choice_without_id"),
-    # url(r'^([0-9]*)$', views.choice, name = "choice"),
-    # url(r'^set/([0-9]*)$', views.set, name = "set"),
     url(r'^choice/$', views.choice, name = "choice"),
     url(r'^set1/$', views.set1, name = "set1"),
     url(r'^set2/$', views.set2, name = "set2"),
