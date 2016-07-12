6bedcea8dba132354218b843f1c18911dc7623f0
Max Ma <hflsmax@gmail.com>
2016-07-12 18:38:54 -0400

    new set2 design

--- saving_app/views.py
+++ saving_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from pymongo import MongoClient
 from django.http import HttpResponse, HttpResponseNotFound
-
+import json
+import os
+import pandas as pd
+from django.conf import settings
 
 def choice_without_id(request):
     return HttpResponseNotFound("<h1>You're not suppose to be here.</h1>")
@@ -24,10 +27,49 @@ def set2(request):
     user_id = request.GET.get("user_id")
     name = request.GET.get("name")
     contri = request.GET.get("contri")
+    age = request.GET.get("age")
+    salary = request.GET.get("salary")
+    savings = request.GET.get("savings")
     if (contri == None):
         contri = -1
     return render(request, 'saving_app/voya-set2.html',
-                    {"user_id": user_id, "name": name, "contri": contri})
+                    {"user_id": user_id, "name": name, "contri": contri,
+                    "age": age, "salary": salary, "savings": savings})
+
+def lookup(request):
+    age = int(request.GET.get("age"))
+    salary = int(request.GET.get("salary"))
+    savings = int(request.GET.get("savings"))
+    csv_file = os.path.join(settings.BASE_DIR, "saving_app/lookup.csv")
+    lk = pd.read_csv(csv_file)
+    # The largest element which is smaller than the query
+    uni_ages = lk.Age.unique()
+    age_thre = uni_ages[age >= uni_ages]
+    age_thre.sort()
+    age_thre = age_thre[-1]
+    # The largest element which is smaller than the query
+    uni_salaries = lk.Salary.unique()
+    salary_thre = uni_salaries[salary >= uni_salaries]
+    salary_thre.sort()
+    salary_thre = salary_thre[-1]
+    # The largest element which is smaller than the query
+    uni_savings = lk.Savings.unique()
+    savings_thre = uni_savings[savings >= uni_savings]
+    savings_thre.sort()
+    savings_thre = savings_thre[-1]
+    small_lk = lk[(lk['Age'] == age_thre) & (lk['Salary'] == salary_thre) & (lk['Savings'] == savings_thre)]
+    lk_dict = {}
+    for row in small_lk.iterrows():
+        lk_dict[row[1]['Rate']] = {}
+        lk_dict[row[1]['Rate']]['target'] = row[1]['Target']
+        lk_dict[row[1]['Rate']]['need'] = row[1]['Need']
+        lk_dict[row[1]['Rate']]['gap'] = row[1]['Gap']
+
+    data = json.dumps(lk_dict)
+    return HttpResponse(data, content_type='application/json')
+
+
+
 
 def set3(request):
     user_id = request.GET.get("user_id")
